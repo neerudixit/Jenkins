8d961ec9b430a0f7dd9d644b368493b737025d96
neerudixit <89036947+neerudixit@users.noreply.github.com>
2021-10-05 02:30:55 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Neeru How r u babe");  
+     System.out.println("Hello Neeru How r u fine");  
     }  
 }
